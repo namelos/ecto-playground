38f7b447876077593eb2925665f4be0cc15e6cbe
Fred Wang <namelos@outlook.com>
2016-12-25 16:32:59 +0800

    add friends repo

--- lib/friends/repo.ex
+++ lib/friends/repo.ex
@@ -1,11 +1,3 @@
 defmodule Friends.Repo do
   use Ecto.Repo, otp_app: :friends
-
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      supervisor(Friends.Repo, [])
-    ]
-  end
 end
