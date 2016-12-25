a89cc8d5057cbc85ab0534ae860fefa482019b24
Fred Wang <namelos@outlook.com>
2016-12-25 14:33:23 +0800

    create person migration

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,14 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :friends, Friends.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "friends_repo",
+#  username: "user",
+#  password: "pass",
+  hostname: "localhost"
+
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
@@ -20,6 +28,7 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :friends, ecto_repos: [Friends.Repo]
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
